0daa51e4e51eadf78b7e8ce68438575c3985ebf9
Susurmi <a.surmann@hotmail.com>
2023-07-21 21:05:49 +0200

    updated setup script

--- setup.sh
+++ setup.sh
@@ -1,8 +1,15 @@
+#!/bin/bash
+
+username=$SUDO_USER
+
+# Update System
+pacman -Syu --noconfirm
+
 # Get needed base dependencies
-sudo pacman -Syu --noconfirm git wget vim kitty neofetch
+pacman -S --noconfirm git wget vim kitty
 
 # Get xorg packages
-sudo pacman -S --noconfirm xorg xorg-server xorg-xinit xdg-utils libx11 libxinerama libxft webkit2gtk
+pacman -S --noconfirm xorg xorg-server xorg-xinit xdg-utils libx11 libxinerama libxft webkit2gtk
 
 # Install Rofi, Picom and feh
 pacman -S --noconfirm rofi picom feh
@@ -11,13 +18,10 @@ pacman -S --noconfirm rofi picom feh
 pacman -S --noconfirm ttf-joypixels terminus-font ttf-font-awesome
 
 # Install YAY
-mkdir -p /home/$ROOT_USER/Downloads
-cd /home/$ROOT_USER/Downloads
-git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si
+su -c 'mkdir -p /home/$ROOT_USER/Downloads && cd /home/$ROOT_USER/Downloads && git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si' $username
 
-# Install Brave Browser
-yay -S --noconfirm brave-bin
+# Install Brave Browser / VS-Codium
+su -c 'yay -S --noconfirm brave-bin && yay -S --noconfirm vscodium-bin' $username
 
 # Get/Set Wallpaper
-wget -P /home/$SUDO_USER/Bilder -O wallpaper.png https://w.wallhaven.cc/full/9m/wallhaven-9mjoy1.png
-feh --bg-fill /home/$SUDO_USER/Bilder/wallpaper.png
+su -c 'wget -P /home/$SUDO_USER/Bilder -O wallpaper.png https://w.wallhaven.cc/full/9m/wallhaven-9mjoy1.png && feh --bg-fill /home/$SUDO_USER/Bilder/wallpaper.png' $username
