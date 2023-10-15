6c54e87e62c2fa95082930356a8fefdabf448f61
Alexander <80062043+Susurmi@users.noreply.github.com>
2023-10-15 12:33:07 +0200

    updated my qtile config

--- dotconfig/qtile/config.py
+++ dotconfig/qtile/config.py
@@ -1,28 +1,3 @@
-# Copyright (c) 2010 Aldo Cortesi
-# Copyright (c) 2010, 2014 dequis
-# Copyright (c) 2012 Randall Ma
-# Copyright (c) 2012-2014 Tycho Andersen
-# Copyright (c) 2012 Craig Barnes
-# Copyright (c) 2013 horsik
-# Copyright (c) 2013 Tao Sauvage
-#
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-#
-# The above copyright notice and this permission notice shall be included in
-# all copies or substantial portions of the Software.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-# SOFTWARE.
 import os
 import subprocess
 from libqtile import bar, layout, widget, hook
@@ -30,10 +5,11 @@ from libqtile.config import Click, Drag, Group, Key, Match, Screen
 from libqtile.lazy import lazy
 from libqtile.utils import guess_terminal
 
-mod = "mod4"
-terminal = guess_terminal()
+from qtile_extras import widget
+from qtile_extras.widget.decorations import PowerLineDecoration
 
-#Colors for the bar
+mod = "mod4"
+terminal = "kitty"
 def init_colors():
     return [["#2e3440", "#2e3440"], # color 0  very dark grayish blue
             ["#3B4252", "#3B4252"], # color 1  dark grayish blue
@@ -41,138 +17,123 @@ def init_colors():
             ["#4C566A", "#4C566A"], # color 3  grayish blue
             ["#D8DEE9", "#D8DEE9"], # color 4  gray
             ["#E5E9F0", "#E5E9F0"], # color 5  white gray
-            ["#ECEFF4", "#ECEFF4"]] # color 6  white
+            ["#ECEFF4", "#ECEFF4"], # color 6  white
+            ["57637c", "57637c"]]   # color 7 light gray
 
 colors = init_colors()
 
 keys = [
-    # A list of available commands that can be bound to keys can be found
-    # at https://docs.qtile.org/en/latest/manual/config/lazy.html
-    # Switch between windows
     Key([mod], "h", lazy.layout.left(), desc="Move focus to left"),
     Key([mod], "l", lazy.layout.right(), desc="Move focus to right"),
     Key([mod], "j", lazy.layout.down(), desc="Move focus down"),
     Key([mod], "k", lazy.layout.up(), desc="Move focus up"),
     Key([mod], "space", lazy.layout.next(), desc="Move window focus to other window"),
-    # Move windows between left/right columns or move up/down in current stack.
-    # Moving out of range in Columns layout will create new column.
     Key([mod, "shift"], "h", lazy.layout.shuffle_left(), desc="Move window to the left"),
     Key([mod, "shift"], "l", lazy.layout.shuffle_right(), desc="Move window to the right"),
     Key([mod, "shift"], "j", lazy.layout.shuffle_down(), desc="Move window down"),
     Key([mod, "shift"], "k", lazy.layout.shuffle_up(), desc="Move window up"),
-    # Grow windows. If current window is on the edge of screen and direction
-    # will be to screen edge - window would shrink.
     Key([mod, "control"], "h", lazy.layout.grow_left(), desc="Grow window to the left"),
     Key([mod, "control"], "l", lazy.layout.grow_right(), desc="Grow window to the right"),
     Key([mod, "control"], "j", lazy.layout.grow_down(), desc="Grow window down"),
     Key([mod, "control"], "k", lazy.layout.grow_up(), desc="Grow window up"),
     Key([mod], "n", lazy.layout.normalize(), desc="Reset all window sizes"),
-    # Toggle between split and unsplit sides of stack.
-    # Split = all windows displayed
-    # Unsplit = 1 window displayed, like Max layout, but still with
-    # multiple stack panes
-    Key(
-        [mod, "shift"],
-        "Return",
-        lazy.layout.toggle_split(),
-        desc="Toggle between split and unsplit sides of stack",
-    ),
+    Key([mod, "shift"], "Return", lazy.layout.toggle_split(), desc="Toggle between split and unsplit sides of stack" ),
     Key([mod], "Return", lazy.spawn(terminal), desc="Launch terminal"),
-    # Toggle between different layouts as defined below
     Key([mod], "Tab", lazy.next_layout(), desc="Toggle between layouts"),
-    Key([mod], "w", lazy.window.kill(), desc="Kill focused window"),
+    Key([mod], "q", lazy.window.kill(), desc="Kill focused window"),
     Key([mod, "control"], "r", lazy.reload_config(), desc="Reload the config"),
     Key([mod, "control"], "q", lazy.shutdown(), desc="Shutdown Qtile"),
-    Key([mod, "shift"], "r", lazy.spawncmd(), desc="Spawn a command using a prompt widget"),
-    Key([mod], "r", lazy.spawn("rofi -show run")),
+    Key([mod], "r", lazy.spawn("rofi -show drun -show-icons")),
     Key([mod], "f", lazy.spawn("firefox")),
     Key([mod], "o", lazy.spawn("obsidian")),
 ]
 
-groups = [Group(i) for i in "123456"]
+groups = []
+
+group_names = ["1", "2", "3", "4", "5", "6"]
+group_labels = ["󰣇", "󰈹", "󰓓", "󰙯", "", ""]
+group_layouts = ["columns", "verticaltile", "columns", "columns", "columns", "columns"]
+
+for i in range(len(group_names)):
+    groups.append(
+        Group(
+            name=group_names[i],
+            layout=group_layouts[i].lower(),
+            label=group_labels[i],
+        ))
 
 for i in groups:
-    keys.extend(
-        [
-            # mod1 + letter of group = switch to group
-            Key(
-                [mod],
-                i.name,
-                lazy.group[i.name].toscreen(),
-                desc="Switch to group {}".format(i.name),
-            ),
-            # mod1 + shift + letter of group = switch to & move focused window to group
-            Key(
-                [mod, "shift"],
-                i.name,
-                lazy.window.togroup(i.name, switch_group=False),
-                desc="Switch to & move focused window to group {}".format(i.name),
-            ),
-            # Or, use below if you prefer not to switch to that group.
-            # # mod1 + shift + letter of group = move focused window to group
-            # Key([mod, "shift"], i.name, lazy.window.togroup(i.name),
-            #     desc="move focused window to group {}".format(i.name)),
-        ]
-    )
+    keys.extend([
+        Key([mod], i.name, lazy.group[i.name].toscreen(), desc="Mod + number to move to that group."),
+        Key(["mod1"], "Tab", lazy.screen.next_group(), desc="Move to next group."),
+        Key(["mod1", "shift"], "Tab", lazy.screen.prev_group(), desc="Move to previous group."),
+        Key([mod, "shift"], i.name, lazy.window.togroup(i.name), desc="Move focused window to new group."),
+    ])
+
+layout_theme = {
+        "border_focus_stack": colors[3], 
+        "border_focus": colors[3],
+        "border_normal": colors[1],
+        "border_width": 3,
+	    "margin": [5,5,5,5],
+}
 
 layouts = [
-    layout.Columns(
-        border_focus_stack=colors[3], 
-        border_focus=colors[3],
-        border_normal=colors[1],
-        border_width=3,
-	    margin=[5,5,5,5],
-        ),
-    layout.Max(
-        border_focus_stack=colors[3], 
-        border_focus=colors[3],
-        border_normal=colors[1],
-        border_width=3,
-	    margin=[5,5,5,5],
-        ),
-    # Try more layouts by unleashing below layouts.
-    # layout.Stack(num_stacks=2),
-    # layout.Bsp(),
-    # layout.Matrix(),
-    # layout.MonadTall(),
-    # layout.MonadWide(),
-    # layout.RatioTile(),
-    # layout.Tile(),
-    # layout.TreeTab(),
-     layout.VerticalTile(
-        border_focus_stack=colors[3], 
-        border_focus=colors[3],
-        border_normal=colors[1],
-        border_width=3,
-	    margin=[5,5,5,5],
-        ),
-    # layout.Zoomy(),
+    layout.Columns(**layout_theme),
+    layout.VerticalTile(**layout_theme),
 ]
 
 widget_defaults = dict(
-    font="sans",
+    font="JetBrainsMono Nerd Font",
     fontsize=12,
     padding=3,
 )
 extension_defaults = widget_defaults.copy()
 
+powerlineRight = {
+    "decorations": [
+        PowerLineDecoration(
+            path= "forward_slash"
+        )
+    ]
+}
+
+powerlineLeft = {
+    "decorations": [
+        PowerLineDecoration(
+            path= "back_slash"
+        )
+    ]
+}
+
 group_box_settings = {
-    "padding": 3,
-    "borderwidth": 4,
-    "active": colors[5],
+    "borderwidth": 3,
+    "active": colors[0],
     "inactive": colors[4],
     "disable_drag": True,
-    "rounded": True,
     "highlight_color": colors[2],
     "block_highlight_text_color": colors[6],
-    "highlight_method": "block",
-    "this_current_screen_border": colors[1],
-    "this_screen_border": colors[1],
-    "other_current_screen_border": colors[1],
-    "other_screen_border": colors[1],
+    "highlight_method": "line",
+    "this_current_screen_border": colors[6],
+    "this_screen_border": colors[6],
+    "other_current_screen_border": colors[7],
+    "other_screen_border": colors[7],
     "foreground": colors[0],
-    "background": colors[0],
+    "background": colors[2],
     "urgent_border": colors[3],
+    "fontsize": 18
+}
+
+seperator_settings = {
+    "background": colors[0],
+    "foreground": colors[3],
+    "linewidth": 1,
+    "padding": 10
+}
+
+clock_settings = {
+    "foreground": colors[4],
+    "fontsize": 14,
 }
 
 screens = [
@@ -183,132 +144,78 @@ screens = [
                     filename='/home/alex/.config/qtile/assets/arch.png',
                     margin=2,
                     mouse_callbacks={'Button1': lazy.shutdown()},
-                ),
-                widget.Sep(
-                    background=colors[0],
-                    foreground=colors[3],
-                    linewidth=1,
-                    padding=10
+                    background= colors[3],
+                    **powerlineLeft
                 ),
                 widget.GroupBox(
                     **group_box_settings,
-                    fontsize=16,
-                ),
-                widget.Sep(
-                    background=colors[0],
-                    foreground=colors[3],
-                    linewidth=1,
-                    padding=10
+                    **powerlineLeft
                 ),
                 widget.Image(
                     filename='/home/alex/.config/qtile/assets/search.png',
                     margin=3,
-                    mouse_callbacks={'Button1': lazy.spawn("rofi -show run")},
+                    background= colors[1],
+                    mouse_callbacks={'Button1': lazy.spawn("rofi -show drun -show-icons")},
+                    **powerlineLeft
                 ),
-                widget.Sep(
-                    background=colors[0],
-                    foreground=colors[3],
-                    linewidth=1,
-                    padding=10
-                ),
-                widget.WindowName(
-                    fontsize=14,
-                ),
-                widget.Sep(
-                    background=colors[0],
-                    foreground=colors[3],
-                    linewidth=1,
-                    padding=10
-                ),
-                widget.CheckUpdates(
-                    fontsize=14,
-                    distro='Arch',
-                    update_interval=3600,
-                    initial_text="Checking for updates!",
-                    restart_indicator="Restart now!",
-                    no_update_string="Up to date!",
-                ),
-                widget.Sep(
-                    background=colors[0],
-                    foreground=colors[3],
-                    linewidth=1,
-                    padding=10
-                ),
+                widget.Spacer(**powerlineRight),
                 widget.OpenWeather(
                     location='',
-                    format='{location_city}: {main_temp}°{units_temperature}',
-                    app_key='',
+                    format='{weather_details} - {main_temp: .0f}°{units_temperature}',
+                    app_key="",
                     language='de',
                     fontsize=14,
                     update_interval=3600,
-                ),
-                widget.Sep(
-                    background=colors[0],
-                    foreground=colors[3],
-                    linewidth=1,
-                    padding=10
+                    background= colors[1],
+                    **powerlineRight
                 ),
                 widget.Clock(
-                    background=colors[0],
-                    foreground=colors[4],
-                    fontsize=14,
+                    **clock_settings,
                     format="%d %B (%A) %H:%M",
-                ),
-                widget.Sep(
-                    background=colors[0],
-                    foreground=colors[3],
-                    linewidth=1,
-                    padding=10
+                    background= colors[2],
+                    **powerlineRight
                 ),
                 widget.CurrentLayoutIcon(
+                    background= colors[3],
                     scale=0.75,
                     padding=5,
                 ),
             ],
             24,
             background=colors[0],
-	        margin=[5, 5, 0, 5]
+	        margin=[0, 0, 0, 0]
         ),
     ),
     Screen(
         top=bar.Bar(
             [
                 widget.Image(
-                    filename='/home/alex/.config/qtile/assets/search.png',
-                    margin=3,
-                    mouse_callbacks={'Button1': lazy.spawn("rofi -show run")},
-                ),
-                widget.Sep(
-                    background=colors[0],
-                    foreground=colors[3],
-                    linewidth=1,
-                    padding=10
+                    filename='/home/alex/.config/qtile/assets/arch.png',
+                    margin=2,
+                    mouse_callbacks={'Button1': lazy.shutdown()},
+                    background= colors[3],
+                    **powerlineLeft
                 ),
                 widget.GroupBox(
                     **group_box_settings,
-                    fontsize=16,
+                    **powerlineLeft
                 ),
-                widget.Sep(
-                    background=colors[0],
-                    foreground=colors[3],
-                    linewidth=1,
-                    padding=10
+                widget.Image(
+                    filename='/home/alex/.config/qtile/assets/search.png',
+                    margin=3,
+                    background= colors[1],
+                    mouse_callbacks={'Button1': lazy.spawn("rofi -show drun -show-icons")},
+                    **powerlineLeft
                 ),
-                widget.WindowName(
-                    fontsize=14,
-                ),
+                widget.Spacer(**powerlineRight),
                 widget.Systray(
-                    background=colors[0],
-                ),
-                widget.Sep(
-                    background=colors[0],
-                    foreground=colors[3],
-                    linewidth=1,
-                    padding=10
+                    background= colors[1],
+                    **powerlineRight
                 ),
                 widget.Image(
                     filename='/home/alex/.config/qtile/assets/mouse.png',
                     margin=4,
+                    background= colors[2],
                 ),
                 widget.GenPollText(
                     name='mouse-battery',
@@ -322,33 +229,24 @@ screens = [
                         'Button1':
                         lazy.widget['mouse-battery'].eval('self.update(self.poll())'),
                     },
-                ),
-                widget.Sep(
-                    background=colors[0],
-                    foreground=colors[3],
-                    linewidth=1,
-                    padding=10
+                    background= colors[2],
+                    **powerlineRight
                 ),
                 widget.Clock(
-                    background=colors[0],
-                    foreground=colors[4],
-                    fontsize=14,
-                    format="%d %B (%A) %H:%M",
-                ),
-                widget.Sep(
-                    background=colors[0],
-                    foreground=colors[3],
-                    linewidth=1,
-                    padding=10
+                    **clock_settings,
+                    format="%H:%M - %d.%m.%y",
+                    background= colors[3],
+                    **powerlineRight
                 ),
                 widget.CurrentLayoutIcon(
+                    background= colors[7],
                     scale=0.75,
                     padding=5,
                     ),
             ],
             24,
             background=colors[0],
-	        margin=[5, 5, 0, 5]
+	        margin=[0, 0, 0, 0]
         ),
     ),
 ]
@@ -367,7 +265,6 @@ bring_front_click = False
 cursor_warp = False
 floating_layout = layout.Floating(
     float_rules=[
-        # Run the utility of `xprop` to see the wm class and name of an X client.
         *layout.Floating.default_float_rules,
         Match(wm_class="confirmreset"),  # gitk
         Match(wm_class="makebranch"),  # gitk
@@ -377,21 +274,15 @@ floating_layout = layout.Floating(
         Match(title="pinentry"),  # GPG key password entry
     ]
 )
+
 auto_fullscreen = True
 focus_on_window_activation = "smart"
 reconfigure_screens = True
+auto_minimize = Truewl_input_rules = None
+wmname = "Qtile"
 
 # Starting the autostart shell script
 @hook.subscribe.startup_once
 def autostart():
     home = os.path.expanduser('~/.config/qtile/scripts/autostart.sh')
     subprocess.Popen([home])
-
-# If things like steam games want to auto-minimize themselves when losing
-# focus, should we respect this or not?
-auto_minimize = True
-
-# When using the Wayland backend, this can be used to configure input devices.
-wl_input_rules = None
-
-wmname = "Qtile"
